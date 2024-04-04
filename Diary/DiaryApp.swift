ff64771e712164045c00454ff4b2588b16055bfc
Nguyen Xuan Duy Anh <duyanh1202@gmail.com>
2024-04-03 22:11:45 -0700

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by Duy Anh Nguyễn Xuân on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
